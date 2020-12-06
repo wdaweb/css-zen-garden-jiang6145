563f4487bf65c853e226dc4baa2804655a1405f8
jiang6145 <jiang.notes14@gmail.com>
2020-12-06 23:14:07 +0800

    css-zen-garden 作業

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 css-zen-garden-jiang6145
+
+## 圖片資源
+
+<span>Photo by <a href="https://unsplash.com/@tavgaksalih?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Tavga K.S</a> on <a href="https://unsplash.com/s/photos/flower?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+
+<span>Photo by <a href="https://unsplash.com/@alexharvey?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Alex Harvey 🤙🏻</a> on <a href="https://unsplash.com/s/photos/flower?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+
+<span>Photo by <a href="https://unsplash.com/@markuswinkler?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Winkler</a> on <a href="https://unsplash.com/s/photos/flower?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+
+<span>Photo by <a href="https://unsplash.com/@tueio?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Kasia Wanner</a> on <a href="https://unsplash.com/s/photos/flower?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+
+<span>Photo by <a href="https://unsplash.com/@sharonp?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sharon Pittaway</a> on <a href="https://unsplash.com/s/photos/flower?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
--- js/index.js
+++ js/index.js
@@ -1,28 +1,101 @@
-let centerX = window.innerWidth / 2;
-let centerY = window.innerHeight / 2;
+$(function () {
+  $('#page-wrapper').fadeIn(1000);
 
-const $banner = $('#banner');
-const $bannerTitle = $('#banner h1');
-const $bannerSubtitle = $('#banner h2');
+  const $mouseStyle = $('.extra1');
+  const $bannerTitle = $('#banner h1');
+  const $bannerSubtitle = $('#banner h2');
+  const $articleTitles = $('h3');
 
-$(window).on('mousemove', function (e) {
-  const moveX = e.clientX - centerX;
-  const moveY = centerY - e.clientY;
-  const color = parseInt(Math.random() * (256 * 256 * 256 - 1)).toString(16);
+  // 各區塊
+  const $zenSummary = $('#zen-summary');
+  const $zenPreamble = $('#zen-preamble');
+  const $zenExplanation = $('#zen-explanation');
+  const $zenParticipation = $('#zen-participation');
+  const $zenBenefits = $('#zen-benefits');
+  const $zenRequirements = $('#zen-requirements');
 
-  $banner.css({
-    transform: `translate(-50%, -50%) rotateY(${moveX / 100}deg)  rotateX(${moveY / 30}deg)`
+  let centerX = window.innerWidth / 2;
+  let centerY = window.innerHeight / 2;
+
+  $(window).on('resize', function () {
+    if (window.innerWidth < 992) {
+      $bannerTitle.removeAttr('style');
+      $bannerSubtitle.removeAttr('style');
+      return;
+    }
+    centerX = window.innerWidth / 2;
+    centerY = window.innerHeight / 2;
   });
 
-  $bannerTitle.css({
-    'text-shadow': `${(moveX / 30) * -1}px ${moveY / 20}px 10px #${color}`
-  });
-  $bannerSubtitle.css({
-    'text-shadow': `${(moveX / 30) * -1}px ${moveY / 20}px 3px #${color}`
-  });
-});
+  $(window).on('mousemove', function (e) {
+    if (window.innerWidth < 992) return;
 
-$(window).on('resize', function () {
-  centerX = window.innerWidth / 2;
-  centerY = window.innerHeight / 2;
+    const moveX = e.clientX - centerX;
+    const moveY = centerY - e.clientY;
+
+    $mouseStyle.css({
+      top: e.clientY + 'px',
+      left: e.clientX + 'px'
+    });
+
+    $bannerTitle.css({
+      'text-shadow': `${(moveX / 30) * -1}px ${moveY / 20}px 10px #f8f9fa41`,
+      transform: `rotateY(${moveX / 100}deg)  rotateX(${moveY / 30}deg)`
+    });
+    $bannerSubtitle.css({
+      'text-shadow': `${(moveX / 30) * -1}px ${moveY / 20}px 3px #f8f9fa41`,
+      transform: `rotateY(${moveX / 100}deg)  rotateX(${moveY / 30}deg)`
+    });
+  });
+
+  $(window).on('scroll', function () {
+    if (window.innerWidth < 992) return;
+    console.log(123);
+    const $scrollIndex = $(window).scrollTop() + window.innerHeight;
+
+    $zenSummary.removeClass('active');
+    $zenPreamble.removeClass('active');
+    $zenExplanation.removeClass('active');
+    $zenParticipation.removeClass('active');
+    $zenBenefits.removeClass('active');
+    $zenRequirements.removeClass('active');
+
+    if ($scrollIndex > $zenRequirements.offset().top + $zenRequirements.height() + 100) {
+      $zenRequirements.addClass('active');
+    } else if ($scrollIndex > $zenBenefits.offset().top + $zenBenefits.height()) {
+      $zenBenefits.addClass('active');
+      $zenParticipation.addClass('active');
+    } else if ($scrollIndex > $zenParticipation.offset().top + $zenParticipation.height() + 200) {
+      $zenParticipation.addClass('active');
+    } else if ($scrollIndex > $zenExplanation.offset().top + $zenExplanation.height()) {
+      $zenPreamble.addClass('active');
+      $zenExplanation.addClass('active');
+    } else if ($scrollIndex > $zenPreamble.offset().top + $zenPreamble.height() + 300) {
+      $zenPreamble.addClass('active');
+      $zenSummary.addClass('active');
+    } else if ($scrollIndex > $zenSummary.offset().top + $zenSummary.height()) {
+      $zenSummary.addClass('active');
+    }
+  });
+
+  $articleTitles.hover(
+    function () {
+      if (window.innerWidth < 992) return;
+
+      $mouseStyle.css({
+        'backdrop-filter': 'invert(1)',
+        width: '28px',
+        height: '28px'
+      });
+    },
+    function () {
+      if (window.innerWidth < 992) return;
+
+      $mouseStyle.css({
+        'backdrop-filter': 'none',
+        width: '24px',
+        height: '24px'
+      });
+    }
+  );
 });
